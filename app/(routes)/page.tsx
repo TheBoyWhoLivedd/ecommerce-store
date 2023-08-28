92c30bc18d251f418741e04376dabb7751140397
Ronnie Ayesigwa <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-08-28 16:47:10 +0300

    Added Billboard and Products cards

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,25 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("732bee04-a88c-4d01-a858-57b831c6f447");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
+import { Urbanist } from 'next/font/google'
 
-const font = Urbanist({ subsets: ["latin"] });
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
-export const metadata: Metadata = {
-  title: "HitajiShop",
-  description: "New Era Technologies",
-};
+import './globals.css'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Store',
+  description: 'Store - The place for all your purchases.',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ToastProvider />
+        <ModalProvider />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  );
+  )
 }
